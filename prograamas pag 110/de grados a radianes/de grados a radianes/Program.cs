3332a232b0a2f3544ba3fd225c0cd2dd134540ae
bryanquinteros1 <79239102+bryanquinteros1@users.noreply.github.com>
2021-02-26 20:27:11 -0600

    Add files via upload

---
+++ prograamas pag 110/de grados a radianes/de grados a radianes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace de_grados_a_radianes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opciones;
+            int total;
+            int r;
+            int g;
+
+
+
+            Console.WriteLine("que tipo de prodcuto es:  1=grados a radiales o 2=radiales a grados");
+            opciones = Console.ReadLine();
+
+            switch (opciones)
+            {
+                case "1":
+                    Console.WriteLine("");
+
+                    Console.WriteLine("ingrese grados:");
+                    g = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    total =  ((int)(g *3.1416/180));
+
+                    Console.WriteLine("el total de  radiales son:" + total);
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("");
+
+
+                    Console.WriteLine("ingrese radiales:");
+                    r = Convert.ToInt32(Console.ReadLine());
+
+
+                    total = (int)(r * 180 / 3.1416);
+
+                    Console.WriteLine("el totla de grados son:" + total);
+
+
+
+
+
+                    break;
+
+
+
+
+            }
+        }
+    }
+}
---
+++ prograamas pag 110/impuesto/impuesto/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace impuesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opciones;
+            int total;
+            int p;
+           
+
+
+            Console.WriteLine("que tipo de prodcuto es:  1-medicinal o 2-otro");
+            opciones = Console.ReadLine();
+
+            switch (opciones)
+            {
+                case "1":
+                    Console.WriteLine("---medicinal--");
+
+                    Console.WriteLine("ingrese el precio del producto:");
+                    p = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    total = p ;
+
+                    Console.WriteLine("no existe impuesto para este producto:" + total);
+
+                    break;
+
+                case "2":
+                    Console.WriteLine("---otro---");
+
+
+                    Console.WriteLine("ingrese el precio del producto:");
+                    p = Convert.ToInt32(Console.ReadLine());
+
+
+                    total = (int)(p / 1.12);
+
+                    Console.WriteLine("el precio del producto sin impuesto:" + total);
+
+
+
+
+
+                    break;
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}

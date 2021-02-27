937b138e0244449b62a904014c4962dc21ad686b
bryanquinteros1 <79239102+bryanquinteros1@users.noreply.github.com>
2021-02-26 21:49:10 -0600

    Add files via upload

---
+++ numero/numero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, r, limite, i, auxiliar;
+            Console.WriteLine(" un numero");
+            limite = int.Parse(Console.ReadLine());
+            a = 0;
+            r = 1;
+            for (i = 0; i < limite; i++)
+            {
+                auxiliar = a;
+                a = r;
+                r = auxiliar + a;
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}

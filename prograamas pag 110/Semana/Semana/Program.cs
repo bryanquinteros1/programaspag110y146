8ee9107683ea9963d83621c9d3f8284c0ded259d
bryanquinteros1 <79239102+bryanquinteros1@users.noreply.github.com>
2021-02-26 20:28:38 -0600

    Add files via upload

---
+++ prograamas pag 110/Semana/Semana/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string numero;
+
+            Console.WriteLine("Un numero del 1 al 7:");
+            numero = Console.ReadLine();
+
+            switch (numero)
+            {
+                case "1":
+                    Console.WriteLine("Lunes.");
+                    break;
+                case "2":
+                    Console.WriteLine("Martes.");
+                    break;
+                case "3":
+                    Console.WriteLine("Miercoles.");
+                    break;
+                case "4":
+                    Console.WriteLine("Jueves.");
+                    break;
+                case "5":
+                    Console.WriteLine("viernes.");
+                    break;
+                case "6":
+                    Console.WriteLine("sabado.");
+                    break;
+                case "7":
+                    Console.WriteLine("Domingo.");
+                    break;
+
+                default:
+
+                    break;
+
+
+
+            }
+        }
+    }
+}

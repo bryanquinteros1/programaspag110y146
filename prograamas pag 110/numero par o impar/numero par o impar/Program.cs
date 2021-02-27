6a4a092eb6d1d92b6f1ff8945bd7fec803091b63
bryanquinteros1 <79239102+bryanquinteros1@users.noreply.github.com>
2021-02-26 20:28:00 -0600

    Add files via upload

---
+++ prograamas pag 110/numero par o impar/numero par o impar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace numero_par_o_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite un numero porfavor:");
+
+            double num = double.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.Write("el numero es par");
+            else
+                Console.Write("el numero es impar");
+
+            Console.ReadKey();
+            
+        }
+    }
+}

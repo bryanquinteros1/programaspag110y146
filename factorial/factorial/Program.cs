f7262bb0dbe11a6b5c4015fcdfadbae3fb0c3b4a
bryanquinteros1 <79239102+bryanquinteros1@users.noreply.github.com>
2021-02-26 20:24:37 -0600

    Add files via upload

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			byte num, i;
+			long result = 1;
+			string linea;
+			Console.Write("Introduce un numero : ");
+			linea = Console.ReadLine();
+			num = byte.Parse(linea);
+			for (i = 2; i <= num; i++)
+			{
+				result = result * i;
+			}
+			Console.WriteLine("El factorial de {0} es {1}", num, result);
+			
+			
+		}
+    }
+}
